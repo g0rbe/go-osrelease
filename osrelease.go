4ffccec3c47135c021fdf0ef28f31fb265ae2363
g0rbe <mail@danielgorbe.com>
2021-05-12 11:13:22 +0200

    Use a global variable

--- osrelease.go
+++ osrelease.go
@@ -8,8 +8,10 @@ import (
 	"strings"
 )
 
-// OSReleasePath contains the default path to the os-release file
-var OSReleasePath string = "/etc/os-release"
+// Path contains the default path to the os-release file
+var Path = "/etc/os-release"
+
+var Release OSRelease
 
 type OSRelease struct {
 	Name string
@@ -37,9 +39,9 @@ type OSRelease struct {
 // Empty lines and comments (beginning with a "#") are ignored.
 func getLines() ([]string, error) {
 
-	output, err := os.ReadFile(OSReleasePath)
+	output, err := os.ReadFile(Path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read %s: %s", OSReleasePath, err)
+		return nil, fmt.Errorf("failed to read %s: %s", Path, err)
 	}
 
 	lines := make([]string, 0)
@@ -72,68 +74,65 @@ func parseLine(line string) (string, string, error) {
 	return subs[0], strings.Trim(subs[1], "\"'"), nil
 }
 
-// Parse parses the os-release file pointing to by OSReleasePath.
-// Return a struct containing the known fields.
-// Fields that not exist will be an empty string.
-func Parse() (OSRelease, error) {
+// Parse parses the os-release file pointing to by Path.
+// The fields are saved into the Release global variable.
+func Parse() error {
 
 	lines, err := getLines()
 	if err != nil {
-		return OSRelease{}, fmt.Errorf("failed to get lines of %s: %s", OSReleasePath, err)
+		return fmt.Errorf("failed to get lines of %s: %s", Path, err)
 	}
-
-	var release OSRelease
 
 	for i := range lines {
 
 		key, value, err := parseLine(lines[i])
 		if err != nil {
-			return OSRelease{}, fmt.Errorf("failed to parse line '%s': %s", lines[i], err)
+			return fmt.Errorf("failed to parse line '%s': %s", lines[i], err)
 		}
 
 		switch key {
 		case "NAME":
-			release.Name = value
+			Release.Name = value
 		case "VERSION":
-			release.Version = value
+			Release.Version = value
 		case "ID":
-			release.ID = value
+			Release.ID = value
 		case "ID_LIKE":
-			release.IDLike = value
+			Release.IDLike = value
 		case "PRETTY_NAME":
-			release.PrettyName = value
+			Release.PrettyName = value
 		case "VERSION_ID":
-			release.VersionID = value
+			Release.VersionID = value
 		case "HOME_URL":
-			release.HomeURL = value
+			Release.HomeURL = value
 		case "DOCUMENTATION_URL":
-			release.DocumentationURL = value
+			Release.DocumentationURL = value
 		case "SUPPORT_URL":
-			release.SupportURL = value
+			Release.SupportURL = value
 		case "BUG_REPORT_URL":
-			release.BugReportURL = value
+			Release.BugReportURL = value
 		case "PRIVACY_POLICY_URL":
-			release.PrivacyPolicyURL = value
+			Release.PrivacyPolicyURL = value
 		case "VERSION_CODENAME":
-			release.VersionCodename = value
+			Release.VersionCodename = value
 		case "UBUNTU_CODENAME":
-			release.UbuntuCodename = value
+			Release.UbuntuCodename = value
 		case "ANSI_COLOR":
-			release.ANSIColor = value
+			Release.ANSIColor = value
 		case "CPE_NAME":
-			release.CPEName = value
+			Release.CPEName = value
 		case "BUILD_ID":
-			release.BuildID = value
+			Release.BuildID = value
 		case "VARIANT":
-			release.Variant = value
+			Release.Variant = value
 		case "VARIANT_ID":
-			release.VariantID = value
+			Release.VariantID = value
 		case "LOGO":
-			release.Logo = value
+			Release.Logo = value
 		default:
-			return OSRelease{}, fmt.Errorf("unknown key found: %s", key)
+			return fmt.Errorf("unknown key found: %s", key)
 		}
 	}
 
-	return release, nil
+	return nil
 }
--- osrelease_test.go
+++ osrelease_test.go
@@ -9,50 +9,50 @@ import (
 // This test is written for Ubuntu 21.04
 func TestParse(t *testing.T) {
 
-	release, err := Parse()
+	err := Parse()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to parse os-relese: %s\n", err)
 		os.Exit(1)
 	}
 
 	switch true {
-	case release.Name != "Ubuntu":
-		t.Errorf("Test failed on NAME: want 'Ubuntu', got '%s'\n", release.Name)
-	case release.Version != "21.04 (Hirsute Hippo)":
-		t.Errorf("Test failed on VERSION: want '21.04 (Hirsute Hippo)', got '%s'\n", release.Version)
-	case release.ID != "ubuntu":
-		t.Errorf("Test failed on ID: want 'ubuntu', got '%s'\n", release.ID)
-	case release.IDLike != "debian":
-		t.Errorf("Test failed on ID_LIKE: want 'debian', got '%s'\n", release.IDLike)
-	case release.PrettyName != "Ubuntu 21.04":
-		t.Errorf("Test failed on PRETTY_NAME: want 'Ubuntu 21.04', got '%s'\n", release.PrettyName)
-	case release.VersionID != "21.04":
-		t.Errorf("Test failed on VERSION_ID: want '21.04', got '%s'\n", release.VersionID)
-	case release.HomeURL != "https://www.ubuntu.com/":
-		t.Errorf("test failed on HOME_URL: want 'https://www.ubuntu.com/', got '%s'\n", release.HomeURL)
-	case release.DocumentationURL != "":
-		t.Errorf("Test failed on DOCUMENTATION_URL: want an empty string, got '%s'\n", release.DocumentationURL)
-	case release.SupportURL != "https://help.ubuntu.com/":
-		t.Errorf("Test failed on SUPPORT_URL: want 'https://help.ubuntu.com/', got '%s'\n", release.SupportURL)
-	case release.BugReportURL != "https://bugs.launchpad.net/ubuntu/":
-		t.Errorf("Test failed on BUG_REPORT_URL: want 'https://bugs.launchpad.net/ubuntu/', got '%s'\n", release.BugReportURL)
-	case release.PrivacyPolicyURL != "https://www.ubuntu.com/legal/terms-and-policies/privacy-policy":
-		t.Errorf("Test failed on PRIVACY_POLICY_URL: want 'https://www.ubuntu.com/legal/terms-and-policies/privacy-policy', got '%s'\n", release.PrivacyPolicyURL)
-	case release.VersionCodename != "hirsute":
-		t.Errorf("Test failed on VERSION_CODENAME: want 'hirsuite', got '%s'\n", release.VersionCodename)
-	case release.UbuntuCodename != "hirsute":
-		t.Errorf("Test failed on UBUNTU_CODENAME: want 'hirsuite', got '%s'\n", release.UbuntuCodename)
-	case release.ANSIColor != "":
-		t.Errorf("Test failed on ANSI_COLOR: want an empty string, got '%s'\n", release.ANSIColor)
-	case release.CPEName != "":
-		t.Errorf("Test failed on CPE_NAME: want an empty string, got '%s'\n", release.CPEName)
-	case release.BuildID != "":
-		t.Errorf("Test failed on BUILD_ID: want an empty string, got '%s'\n", release.BuildID)
-	case release.Variant != "":
-		t.Errorf("Test failed on VARIANT: want an empty string, got '%s'\n", release.Variant)
-	case release.VariantID != "":
-		t.Errorf("Test failed on VARIANT_ID: want an empty string, got '%s'\n", release.VariantID)
-	case release.Logo != "":
-		t.Errorf("Test failed on LOGO: want an empty string, got '%s'\n", release.Logo)
+	case Release.Name != "Ubuntu":
+		t.Errorf("Test failed on NAME: want 'Ubuntu', got '%s'\n", Release.Name)
+	case Release.Version != "21.04 (Hirsute Hippo)":
+		t.Errorf("Test failed on VERSION: want '21.04 (Hirsute Hippo)', got '%s'\n", Release.Version)
+	case Release.ID != "ubuntu":
+		t.Errorf("Test failed on ID: want 'ubuntu', got '%s'\n", Release.ID)
+	case Release.IDLike != "debian":
+		t.Errorf("Test failed on ID_LIKE: want 'debian', got '%s'\n", Release.IDLike)
+	case Release.PrettyName != "Ubuntu 21.04":
+		t.Errorf("Test failed on PRETTY_NAME: want 'Ubuntu 21.04', got '%s'\n", Release.PrettyName)
+	case Release.VersionID != "21.04":
+		t.Errorf("Test failed on VERSION_ID: want '21.04', got '%s'\n", Release.VersionID)
+	case Release.HomeURL != "https://www.ubuntu.com/":
+		t.Errorf("test failed on HOME_URL: want 'https://www.ubuntu.com/', got '%s'\n", Release.HomeURL)
+	case Release.DocumentationURL != "":
+		t.Errorf("Test failed on DOCUMENTATION_URL: want an empty string, got '%s'\n", Release.DocumentationURL)
+	case Release.SupportURL != "https://help.ubuntu.com/":
+		t.Errorf("Test failed on SUPPORT_URL: want 'https://help.ubuntu.com/', got '%s'\n", Release.SupportURL)
+	case Release.BugReportURL != "https://bugs.launchpad.net/ubuntu/":
+		t.Errorf("Test failed on BUG_REPORT_URL: want 'https://bugs.launchpad.net/ubuntu/', got '%s'\n", Release.BugReportURL)
+	case Release.PrivacyPolicyURL != "https://www.ubuntu.com/legal/terms-and-policies/privacy-policy":
+		t.Errorf("Test failed on PRIVACY_POLICY_URL: want 'https://www.ubuntu.com/legal/terms-and-policies/privacy-policy', got '%s'\n", Release.PrivacyPolicyURL)
+	case Release.VersionCodename != "hirsute":
+		t.Errorf("Test failed on VERSION_CODENAME: want 'hirsuite', got '%s'\n", Release.VersionCodename)
+	case Release.UbuntuCodename != "hirsute":
+		t.Errorf("Test failed on UBUNTU_CODENAME: want 'hirsuite', got '%s'\n", Release.UbuntuCodename)
+	case Release.ANSIColor != "":
+		t.Errorf("Test failed on ANSI_COLOR: want an empty string, got '%s'\n", Release.ANSIColor)
+	case Release.CPEName != "":
+		t.Errorf("Test failed on CPE_NAME: want an empty string, got '%s'\n", Release.CPEName)
+	case Release.BuildID != "":
+		t.Errorf("Test failed on BUILD_ID: want an empty string, got '%s'\n", Release.BuildID)
+	case Release.Variant != "":
+		t.Errorf("Test failed on VARIANT: want an empty string, got '%s'\n", Release.Variant)
+	case Release.VariantID != "":
+		t.Errorf("Test failed on VARIANT_ID: want an empty string, got '%s'\n", Release.VariantID)
+	case Release.Logo != "":
+		t.Errorf("Test failed on LOGO: want an empty string, got '%s'\n", Release.Logo)
 	}
 }
